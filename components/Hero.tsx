71b5887729a20d4d2661c595e2bf66c00d9b5694
BootstrapKat <bootstrapkatanderson@gmail.com>
2023-09-18 15:31:56 -0400

    feat: More hero module edits

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,10 +1,32 @@
 import styles from "./Hero.module.css";
+import Image from "next/image";
 
 const Hero = () => {
   return (
-    <div>
-      <h1 className={styles.heading}>This is the hero</h1>
-    </div>
+    <>
+      <div className={styles.hero}>
+        <div className={styles.heroContent}>
+          <h1 className={styles.heading}>A Simple Bookmark Manager</h1>
+          <p className={styles.heroBody}>
+            A clean and simple interface to organize your favourite websites.
+            Open a new browser tab and see your sites load instantly. Try it for
+            free.
+          </p>
+          <div className={styles.ctas}>
+            <button className={styles.darkBtn}>Get it on Chrome</button>
+            <button className={styles.lightBtn}>Get it on Firefox</button>
+          </div>
+        </div>
+        <Image
+          src="/illustration-hero.svg"
+          alt="Bookermark Illustration Hero"
+          height={500}
+          width={700}
+          priority
+        />
+      </div>
+      <div className={styles.heroBg}></div>
+    </>
   );
 };
 
